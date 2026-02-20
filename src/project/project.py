07b3152c33c6e0b9c2dbcd14d6e6b8532f6d76c8
rainclouded <155860244+rainclouded@users.noreply.github.com>
2026-02-20 13:58:45 -0600

    image processing adding

--- src/project/project.py
+++ src/project/project.py
@@ -1,1 +1,16 @@
-import gradio as gr
+from PIL import Image, ImageOps
+import numpy as np
+side_length = 512
+
+def resize_image(image_name):
+     # We cant have too large an image for now
+    resized  = ImageOps.cover(image_name, side_length, (0.5, 0.5))
+    return resized
+
+def convert_greybits(image:Image.Image):
+    grey_image = np.zeroes(side_length,side_length)
+    for x in side_length:
+        for y in side_length:
+            R,G,B = image[x,y]
+            grey_image[x,y] = 0.299 * R + 0.587 * G + 0.114*B
+
